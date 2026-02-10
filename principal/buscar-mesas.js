950c0471836b71e344c01cbec9be09b5b38408d5
Níckollas <149337698+fryh455@users.noreply.github.com>
2026-02-10 16:49:57 -0300

    Update buscar-mesas.js

--- principal/buscar-mesas.js
+++ principal/buscar-mesas.js
@@ -60,7 +60,7 @@ if (typeof window !== "undefined") {
     const nome = document.getElementById("nome-mesa").value;
     try {
       const res = await criarMesa(nome);
-      window.location.href = `/mesa/mesa.html?mid=${res.mesaId}`;
+      window.location.href = `mesa/mesa.html?mid=${res.mesaId}`;
     } catch (e) {
       alert(e.message);
     }
@@ -77,7 +77,7 @@ if (typeof window !== "undefined") {
       btn.textContent = "Entrar";
       btn.addEventListener("click", async () => {
         await entrarEmMesa(m.mesaId);
-        window.location.href = `./mesa/mesa.html?mid=${m.mesaId}`;
+        window.location.href = `mesa/mesa.html?mid=${m.mesaId}`;
       });
       li.appendChild(btn);
       ul.appendChild(li);
@@ -85,3 +85,4 @@ if (typeof window !== "undefined") {
   });
 
 }
+
